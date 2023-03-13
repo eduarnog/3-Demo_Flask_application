0abc187725a5193cbfb505b54d349ae07556157e
eduarnog <126154429+eduarnog@users.noreply.github.com>
2023-03-13 16:24:48 +0100

    v.001.004

--- website/__init__.py
+++ website/__init__.py
@@ -1,20 +1,26 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
-
+##from flask_migrate import Migrate
+from os import path
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
 
+
 def create_app():
     app = Flask(__name__)
+    ##app.config.from_object('config')
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
+    ##migrate = Migrate(app,db)
+    
+    img = '.static/icons/menu_24px.svg'
 
     from .views import views
     from .auth import auth
+    
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
@@ -23,7 +29,8 @@ def create_app():
     
     with app.app_context():
         db.create_all()
-
+        
+    
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
@@ -31,8 +38,8 @@ def create_app():
     @login_manager.user_loader
     def load_user(id):
         user = User.query.get(int(id))
-        
-        
+           
+  
     return app
 
 
@@ -40,3 +47,4 @@ def create_database(app):
     if not path.exists('website/' + DB_NAME):
         db.create_all(app=app)
         print('Created Database!')
+        
--- website/views.py
+++ website/views.py
@@ -1,15 +1,12 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from . import db
-import json
 
 views = Blueprint('views', __name__)
-
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("01-home.html", user=current_user)
-
 
 @views.route('/dashboard', methods=['GET', 'POST'])
 @login_required
